672cba8d5b7a48ac0030ed072f5720d7e39ef32e
Gabriel Andrade <gabriel.andrade200p@gmail.com>
2021-08-24 15:10:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Semaforo from './components/Semaforo'
+import './App.css'
+
+export default function App() {
+
+  return(
+    <>
+    <h1>Nome: Gabriel Andrade Pereira | RM: 87564 | Turma: 1TDSH</h1>
+    <Semaforo/>
+    </>
+  )
+}
+
